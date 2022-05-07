1a82894869f437ec8822a2d86bc43a41fc985aee
user <mail>
2022-05-07 18:21:36 +0300

    one

---
+++ move.js
@@ -0,0 +1,29 @@
+
+fetch("base.json")
+
+.then(function(response){
+	return response.json();
+})
+
+.then(function(base) {
+	let placeholder = document.querySelector("#in-table");
+	let out = "";
+	for(let uni of base){
+	  out += `
+		<tr>
+	    <td>${uni.FIO}</td>
+	    <td>${uni.Telephon}</td>
+	    <td>${uni.Email}</td>
+	    <td>${uni.Adress}</td>
+	    <td>${uni.Location}</td>
+	    <td>${uni.Login}</td>
+			<td>${uni.Password}</td>
+			<td>${uni.Model}</td>
+			<td>${uni.Operator}</td>
+			<td>${uni.Phone}</td>
+	  </tr>
+	  `;
+	}
+	placeholder.innerHTML = out;
+})
+
